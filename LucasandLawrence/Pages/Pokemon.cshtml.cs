cde29e20882464b18c72fcf6b5a475ec191f90a4
Huaping Liu (CSI Interfusion Inc) <v-huli@microsoft.com>
2019-10-09 19:05:06 -0700

    Add project files.

---
+++ LucasandLawrence/Pages/Pokemon.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LucasandLawrence.Pages
+{
+    public class PokemonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
